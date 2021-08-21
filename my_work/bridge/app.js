303d13c59ee5fe9e5305250b24a9babdeb0fb1cb
Sejo22 <sead_sijerkic@hotmail.com>
2021-08-21 10:42:33 +0200

    bridge preview

---
+++ my_work/bridge/app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+  
