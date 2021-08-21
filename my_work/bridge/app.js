212846aa7b0b55ae9e63aca72382b63b269d08c0
Sejo22 <sead_sijerkic@hotmail.com>
2021-08-21 20:54:44 +0200

    bridhe update

--- my_work/bridge/app.js
+++ my_work/bridge/app.js
@@ -1,11 +1,17 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
+const swiper = new Swiper('.slider1', {
     direction: 'horizontal',
     loop: true,
     
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    }
+    },
   });
   
+
+  const swiper2 = new Swiper('.slider2', {
+    direction: 'horizontal',
+    slidesPerView:5,
+    freeMode: true,
+
+  });
