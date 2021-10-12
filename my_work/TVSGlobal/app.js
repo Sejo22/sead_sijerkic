d0471b0d864073919e7b206789dfff07ab4b18cd
Sejo22 <sead_sijerkic@hotmail.com>
2021-10-12 17:52:54 +0200

    TVS Global update 12.10.2021

--- my_work/TVSGlobal/app.js
+++ my_work/TVSGlobal/app.js
@@ -8,15 +8,20 @@ $(window).scroll(function(){
 
 });
 
+
 const navToggler = document.querySelector('.navbar-toggler');
+const navUlOpen = document.querySelector('ul');
 let navOpen = false;
 navToggler.addEventListener('click', () => {
     if (!navOpen) {
         navToggler.classList.add('open');
         navOpen = true;
+        navUlOpen.classList.add('open');
     }
     else {
         navToggler.classList.remove('open');
         navOpen = false;
+        navUlOpen.classList.remove('open');
+        
     }
 });
