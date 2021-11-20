83e41c8cf5c922d962e7e8e28a4681b2feec2ca9
Sejo22 <sead_sijerkic@hotmail.com>
2021-11-20 23:14:51 +0100

    E-lab update

---
+++ my_work/E-lab/app.js
@@ -0,0 +1,62 @@
+// MENU 
+const navToggler = document.querySelector('.nav-toggler');
+const navList = document.querySelector('ul');
+let navOpen = false;
+navToggler.addEventListener('click', () => {
+    if (!navOpen) {
+        navToggler.classList.add('open');
+        navOpen = true;
+        navList.classList.add('open');
+        document.body.classList.add('scroll-lock');
+
+    }
+    else {
+        navToggler.classList.remove('open');
+        navOpen = false;
+        navList.classList.remove('open');
+        document.body.classList.remove('scroll-lock');
+    }
+});
+
+// HIDE MENU
+const menuLink = document.querySelectorAll('.nav-link');
+
+menuLink.forEach(function(mL){
+    mL.addEventListener('click',() => {
+        navToggler.classList.remove('open');
+        navList.classList.remove('open');
+        navOpen = false;
+        document.body.classList.remove('scroll-lock');
+    });
+});
+
+// SCROLL RESET 
+
+window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+}
+
+// STICKY NAV
+window.onscroll = function() {stickyNav()};
+const header = document.getElementById('header');
+let sticky = header.offsetTop;
+
+function stickyNav() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    }
+    else {
+        header.classList.remove('sticky');
+    }
+}
+
+// SMOOTH ON CLICK
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
