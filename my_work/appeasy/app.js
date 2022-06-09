d19fd2f9e14eeba99a89db7bb1affdd6e34c358a
Sead Šijerkić <sead_sijerkic@hotmail.com>
2022-06-09 10:24:35 +0200

    Appeasy update

--- my_work/appeasy/app.js
+++ my_work/appeasy/app.js
@@ -8,6 +8,7 @@ const swiper = new Swiper('.swiper', {
     },
     breakpoints: {
       320: {
+        loop: true,
         slidesPerView: 1,
         spaceBetween:30,
         speed: 15000,
@@ -15,12 +16,14 @@ const swiper = new Swiper('.swiper', {
 
       },
       532: {
+        loop: true,
         slidesPerView: 2,
         spaceBetween: 30,
         speed: 10000,
         grabCursor:true,
       },
       1200: {
+        loop: true,
         grabCursor:true,
         slidesPerView: 3,
         spaceBetween: 32,
@@ -28,5 +31,25 @@ const swiper = new Swiper('.swiper', {
       }
     },
   });
+
+  
+// NAV TOGGLE
+const navToggler = document.querySelector('.nav-toggler');
+const navUlOpen = document.querySelector('ul');
+let navOpen = false;
+navToggler.addEventListener('click', () => {
+    if (!navOpen) {
+        navToggler.classList.add('open');
+        navOpen = true;
+        navUlOpen.classList.add('open');
+        document.body.classList.add('scroll-lock');
+    }
+    else {
+        navToggler.classList.remove('open');
+        navOpen = false;
+        navUlOpen.classList.remove('open'); 
+        document.body.classList.remove('scroll-lock');
+    }
+});
   
   
